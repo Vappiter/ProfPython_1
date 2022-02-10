9392aeebf2f8c943782aad55d537b3e72fe0ede0
Andrey Vladimirov <vappiter@yandex.ru>
2022-02-10 23:54:08 +0300

    main task YES

---
+++ main.py
@@ -0,0 +1,8 @@
+from application.salary import calculate_salary
+from application.db.get_employees import get_employees
+
+
+if __name__ == '__main__':
+ print('Modul MAIN')   
+ calculate_salary()
+ get_employees()
