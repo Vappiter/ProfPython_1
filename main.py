055fcdfa20e17f9ca5d3e016036b7d7699ee5808
Andrey Vladimirov <vappiter@yandex.ru>
2022-02-11 00:07:04 +0300

    all task YES

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from application.salary import calculate_salary
 from application.db.get_employees import get_employees
+from datetime import date
+from pprint import pprint
 
 
 if __name__ == '__main__':
+ 
+ pprint (f'Current date - {date.today()}')
  print('Modul MAIN')   
  calculate_salary()
  get_employees()
